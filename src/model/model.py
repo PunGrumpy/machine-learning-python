0f542434a46aad4dce818748248c0b60ca01b98f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-18 19:39:05 +0700

    :sparkles: add logging on model

--- src/model/model.py
+++ src/model/model.py
@@ -1,27 +1,35 @@
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 import os
 import sys
+from loguru import logger
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from utils.load_and_save import save_model
+from utils.load_and_save import save_model, save_numpy_array
 
 
 def train_model():
+    logger.info("Loading and preparing data...")
     X, y = load_diabetes(return_X_y=True)
+    logger.debug(f"Data shape: {X.shape}")
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    model = LinearRegression()
+    model = RandomForestRegressor()
+    logger.info("Training model...")
     model.fit(X_train, y_train)
     return model, X_test, y_test
 
 
 def main():
     model, X_test, y_test = train_model()
+    logger.info("Saving model and test data...")
     save_model(model, "models/model.pkl")
+    save_numpy_array(X_test, "data_raw/X_test.npy")
+    save_numpy_array(y_test, "data_raw/y_test.npy")
 
 
 if __name__ == "__main__":
+    logger.add("model.log")
     main()
--- src/model/validate_model.py
+++ src/model/validate_model.py
@@ -1,27 +1,33 @@
 from sklearn.metrics import mean_squared_error
 import os
 import sys
+from loguru import logger
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.load_and_save import load_model, load_numpy_array
 
 
 def validate_model(model, X_test, y_test):
+    logger.info("Making predictions...")
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
     return mse
 
 
 def main():
+    logger.info("Loading model and test data...")
     model = load_model("models/model.pkl")
     # load test data
     X_test = load_numpy_array("data_raw/X_test.npy")
     y_test = load_numpy_array("data_raw/y_test.npy")
+    logger.debug(f"Test data shape: {X_test.shape}")
     mse = validate_model(model, X_test, y_test)
-    print(f"Model performance: {mse}")
-    if mse > 3000:
+    logger.info(f"Model performance: {mse}")
+    if mse > 4000:
         raise Exception("Model performance does not meet the threshold")
+    logger.info("Model performance meets the threshold")
 
 
 if __name__ == "__main__":
+    logger.add("validate_model.log")
     main()
